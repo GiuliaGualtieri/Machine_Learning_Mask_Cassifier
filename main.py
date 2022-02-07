3ad701a0fab7f540e903f69fa830428e49e1fea5
GiuliaGualtieri <giulia.gualtieri@mail.polimi.it>
2022-02-07 21:44:55 +0100

    update model to version 1.1.0 add data test_image_8, _9, _10 modified file gitignore

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 import cv2
 import numpy as np
 import pickle
-import socket, pickle
+import socket
+import joblib
 from PIL import Image
 
-with open("models/model.pkl", 'rb') as f:
-  model_mask = pickle.load(f)
+# load the model
+#with open("models/modelv1.pkl", 'rb') as f:
+  #model_mask = pickle.load(f)
+model_mask = joblib.load("models/modelv.1.1.0.pkl")
 
 # define the target class
-class_names = ["no-mask", "mask"]
+class_names =  ["mask", "no-mask"]
 
-filepath = "data/test_image_2.jpg"
+filepath = "data/test_image_1.jpg"
+img = Image.open('data/test_image_1.jpg')
+img.show()
 im = cv2.imread(filepath, cv2.IMREAD_COLOR)
 
 # La colormap e BGR convertila a RGB per visualizzazione correta
@@ -20,19 +25,23 @@ im =cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 # Resize Immagine
 im = cv2.resize(im, (64,64))
 
-img = Image.open('data/test_image_2.jpg')
-img.show()
-
 im=im.reshape(1,-1)
 #im.shape
 
 y_hat=model_mask.predict_proba(im)
 #y_hat
 
-idx = np.argmax(y_hat)
+#if y_hat[0,1]>0.10:
+#  idx=1
+#else:
+#  idx=0
 
+idx = np.argmax(y_hat)
+#print(y_hat.shape) 
 
 cv2.waitKey(300)
 #print(idx)
-print(class_names[idx])
+print('Prob of having a mask: ', y_hat[0,0])
+print('Prob of not having a mask: ', y_hat[0,1])
+print('answer is: ', class_names[idx])
 
